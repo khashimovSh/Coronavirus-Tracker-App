e730e7a57c38bbd2aa84083b784ff29a666a11e4
Shakhzod Khashimov <shoh.hashimov.99@gmail.com>
2020-10-29 15:07:21 +0100

    all files are added

---
+++ src/test/java/io/shakhzod/coronavirus/CoronavirusTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.shakhzod.coronavirus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
